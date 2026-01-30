8edabf6bce9337a8d7b12bd79ff3e9424fccec85
Shushil maddeshiya <maddeshiyashushil01@gmail.com>
2026-01-30 09:45:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+
 const taskInput = document.getElementById('task-input');
 const prioritySelect = document.getElementById('priority-select');
 const addBtn = document.getElementById('add-btn');
